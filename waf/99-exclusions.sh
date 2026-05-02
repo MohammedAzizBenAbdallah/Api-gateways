4fdb0d415393b357b6e68e422e3144aa2d112107
MohammedAzizBenAbdallah <abdallahaziz2004@gmail.com>
2026-05-02 10:16:58 +0100

    fixed firewall blocking js files form loading

--- waf/99-exclusions.sh
+++ waf/99-exclusions.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
+
+# Exclude PostHog cookies from SQL injection rule
 echo 'SecRuleUpdateTargetById 942290 "!REQUEST_COOKIES:/^ph_/"' \
+  >> /etc/modsecurity.d/owasp-crs/rules/REQUEST-999-COMMON-EXCEPTIONS-AFTER.conf
+
+# Exclude Vite dev assets from .js extension restriction (rule 920440)
+echo 'SecRule REQUEST_URI "@beginsWith /node_modules/" "id:1001,phase:1,pass,nolog,ctl:ruleRemoveById=920440"' \
+  >> /etc/modsecurity.d/owasp-crs/rules/REQUEST-999-COMMON-EXCEPTIONS-AFTER.conf
+
+# Exclude Vite's own source files too (e.g. /src/*.jsx triggers referrer checks)
+echo 'SecRule REQUEST_URI "@beginsWith /src/" "id:1002,phase:1,pass,nolog,ctl:ruleRemoveById=920440"' \
+  >> /etc/modsecurity.d/owasp-crs/rules/REQUEST-999-COMMON-EXCEPTIONS-AFTER.conf
+
+# Exclude /@vite/ and /@fs/ internal Vite paths
+echo 'SecRule REQUEST_URI "@beginsWith /@" "id:1003,phase:1,pass,nolog,ctl:ruleRemoveById=920440"' \
   >> /etc/modsecurity.d/owasp-crs/rules/REQUEST-999-COMMON-EXCEPTIONS-AFTER.conf
